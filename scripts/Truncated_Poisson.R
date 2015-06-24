0b993acffcf899883507582d9b0cc7591ec8c349
Rafael S. de Souza <rafael.2706@gmail.com>
2015-06-24 18:50:02 +0200

    Truncated NB, another test (1,10)

--- scripts/Truncated_Poisson.R
+++ scripts/Truncated_Poisson.R
@@ -13,16 +13,17 @@ require(gdata)
 require(runjags)
 require(gdata)
 # Data
-exo_dat<-read.table("../take_data/system_catalogue.dat",sep="\t",header=TRUE)
+exo_dat0<-read.table("../take_data/system_catalogue.dat",sep="\t",header=TRUE)
 # select for now Period,  Planet radius, Planet mass
 
-exo_dat<-exo_dat[complete.cases(exo_dat[,c("n_planets","star_radius","star_metallicity")]),]
+exo_dat<-exo_dat0[complete.cases(exo_dat0[,c("n_planets","star_radius","star_metallicity","star_temperature")]),]
 y<-exo_dat$n_planets
 #x1<-exo_dat$star_temperature
-x1<-exo_dat$star_radius
-x2<-exo_dat$star_metallicity
+x1<-scale(exo_dat$star_radius)
+x2<-scale(exo_dat$star_metallicity)
+x3<-scale(exo_dat$star_temperature)
 
-X <- model.matrix(~x1 + x2)
+X <- model.matrix(~x1 + x2+x3)
 K <- ncol(X)
 N<-nrow(exo_dat)
 
@@ -44,7 +45,7 @@ cat("
     beta  ~ dmnorm(bet0[], Bet0[,])  
     # Prior for size
 
-    size~dunif(0.001,10)
+    size~dgamma(0.001,0.001)
     #########
     #Likelihood 
     for (i in 1:N) {
@@ -52,30 +53,74 @@ cat("
     log(mu[i])<-max(-20,min(20,eta[i]))# Ensures that large beta values do not cause numerical problems. 
 
      p[i]<-size/(size+mu[i])
-     Y[i]~dnegbin(p[i],size)T(1,)
+     Y[i]~dnegbin(p[i],size)T(1,10)
 #    Y[i]~dpois(mu[i])T(1,)
     # Prediction
     Pred[i]~dnegbin(p[i],size)T(1,10)
-    }          
+# Discrepancy measures
+YNew[i] ~dnegbin(p[i],size)T(1,10)
+    expY[i] <- mu[i]
+    varY[i] <- mu[i] + pow(mu[i],2) / size
+    PRes[i] <-(Y[i] - expY[i])/sqrt(varY[i])
+    PResNew[i] <-(YNew[i] - expY[i])/sqrt(varY[i])
+    D[i]<-pow(PRes[i],2)
+    DNew[i]<-pow(PResNew[i],2)
+    }
+Fit<-sum(D[1:N])
+New<-sum(DNew[1:N])
     }
     ",fill = TRUE)
 sink()
 #######
 
 #Inits function
-inits1  <- function () {
-  list(beta  = rnorm(K, 0, 0.01)#,  #Regression parameters
-       #phi   = 0
+inits0  <- function () {
+  list(beta  = rnorm(K, 0, 0.01),  #Regression parameters
+       size =runif(1,0.01,10)
   )  }
+
+inits1 <- inits0() 
+inits2 <- inits0() 
+inits3 <- inits0() 
 
 #Parameters to estimate
 params1 <- c("beta", 
-             "Pred"
+             "Pred",
+             "PRes",
+             "Fit",
+             "New"
 )
+
+library(parallel)
+cl <- makeCluster(3)
+GP1 <- run.jags(method="rjparallel", method.options=list(cl=cl),
+                     data = win.data1, 
+                     inits = list(inits1,inits2,inits3),
+                     model="GPGLM.txt",
+                     n.chains = 3,
+                     adapt=1000,
+                     monitor=c(params1),
+                     burnin=5000,
+                     sample=10000,
+                     summarise=FALSE,
+                     plots=FALSE
+)
+
+jagssamples.nb <- as.mcmc.list(GP1)
+pred.NBerrx<-summary(as.mcmc.list(GP1,vars="Pred"),quantiles=c(0.005,0.025,0.25,0.5,0.75,0.975, 0.995))
+
+
+
+gsamples<-ggs(jagssamples,family="beta")
+
+ggs_density(gsamples)
+
+
+
 
 #Start Gibbs sampler
 GP   <- jags(data       = win.data1,
-             inits      = inits1,
+             inits      = inits,
              parameters = params1,
              model      = "GPGLM.txt",
              n.thin     = 10,
@@ -91,7 +136,5 @@ jagssamples <- as.mcmc(GP)
 pred_exo<-summary(as.mcmc.list(jagssamples, vars="Pred"),quantiles=c(0.005,0.5,0.975))
 
 
-gsamples<-ggs(jagssamples)
 
-ggs_density(gsamples)
 
